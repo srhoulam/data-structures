7b5a3578ecc78842651068fad6fa352d2cd6d784
Saad Rhoulam <saad@rhoulam.com>
2016-01-16 17:46:08 -0500

    Have add/sub tests also cover adding with differing signs.

--- spec/LargeIntegerSpec.js
+++ spec/LargeIntegerSpec.js
@@ -39,6 +39,10 @@ describe("large integer", function() {
 
             expect(LI.add(lint1, lint2).toString()).toBe('+0');
             expect(LI.subtract(lint1, lint2).toString()).toBe('-0');
+
+            lint2.setNegative();
+
+            expect(LI.add(lint1, lint2).toString()).toBe('-0');
         });
         it('0 +/- 1', function() {
             lint1.addDigit(0);
@@ -47,6 +51,10 @@ describe("large integer", function() {
 
             expect(LI.add(lint1, lint2).toString()).toBe('+1');
             expect(LI.subtract(lint1, lint2).toString()).toBe('-1');
+
+            lint2.setNegative();
+
+            expect(LI.add(lint1, lint2).toString()).toBe('-1');
         });
         it('1 +/- 1', function() {
             lint1.addDigit(1);
@@ -55,6 +63,10 @@ describe("large integer", function() {
 
             expect(LI.add(lint1, lint2).toString()).toBe('+2');
             expect(LI.subtract(lint1, lint2).toString()).toBe('-0');
+
+            lint2.setNegative();
+
+            expect(LI.add(lint1, lint2).toString()).toBe('-0');
         });
         it('123 +/- 123', function() {
             lint1.addDigit(1);
@@ -67,6 +79,10 @@ describe("large integer", function() {
 
             expect(LI.add(lint1, lint2).toString()).toBe('+246');
             expect(LI.subtract(lint1, lint2).toString()).toBe('-000');
+
+            lint2.setNegative();
+
+            expect(LI.add(lint1, lint2).toString()).toBe('-000');
         });
         it('25 +/- 50', function() {
             lint1.addDigit(2);
@@ -77,6 +93,10 @@ describe("large integer", function() {
 
             expect(LI.add(lint1, lint2).toString()).toBe('+75');
             expect(LI.subtract(lint1, lint2).toString()).toBe('-25');
+
+            lint2.setNegative();
+
+            expect(LI.add(lint1, lint2).toString()).toBe('-25');
         });
         it('9000 +/- 9001', function() {
             lint1.addDigit(9);
@@ -91,6 +111,10 @@ describe("large integer", function() {
 
             expect(LI.add(lint1, lint2).toString()).toBe('+18001');
             expect(LI.subtract(lint1, lint2).toString()).toBe('-0001');
+
+            lint2.setNegative();
+
+            expect(LI.add(lint1, lint2).toString()).toBe('-0001');
         });
         it('16000000000000 +/- 50000', function() {
             lint1.addDigit(1);
@@ -116,6 +140,10 @@ describe("large integer", function() {
 
             expect(LI.add(lint1, lint2).toString()).toBe('+16000000050000');
             expect(LI.subtract(lint1, lint2).toString()).toBe('+15999999950000');
+
+            lint2.setNegative();
+
+            expect(LI.add(lint1, lint2).toString()).toBe('+15999999950000');
         });
     });
 });
